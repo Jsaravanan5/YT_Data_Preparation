ecab5ebb1d70af0b981a6709a1d0992412514be7
J Saravanan <160335056+Jsaravanan5@users.noreply.github.com>
2024-06-06 13:24:31 +0000

    Streamlit Version 1.2

--- Sample_Streamlit.py
+++ Sample_Streamlit.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from googleapiclient.discovery import build
+#from googleapiclient.discovery import build
 
 def main():
     st.title(":red[YOUTUBE HARVESTING AND DATA WAREHOUSING]")
@@ -10,6 +10,6 @@ def main():
     st.write(".")
     st.header(":question:&:pencil: Queries & Result")
     st.write("channel details,playlist details,comment details and video details")
-    st.page_link("https://zany-couscous-5gvg9v9wqq4r3v4r9-8501.app.github.dev/",icon='👉',label=':red-background[CLICK HERE]')
+    st.page_link("https://zany-couscous-5gvg9v9wqq4r3v4r9-8502.app.github.dev/",icon='👉',label=':red-background[CLICK HERE]')
 if __name__ == "__main__":
     main()
