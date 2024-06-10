b881cae6f84da04d43dc55a3653c2916036795b7
J Saravanan <160335056+Jsaravanan5@users.noreply.github.com>
2024-06-10 09:46:41 +0000

    update 1.1

--- Streamlit_Query_Result.py
+++ Streamlit_Query_Result.py
@@ -46,7 +46,7 @@ def execute_query(query):
 
 
 st.image("https://upload.wikimedia.org/wikipedia/commons/9/90/Logo_of_YouTube_%282013-2015%29.svg", caption=None, width=None, use_column_width=None, clamp=False, channels='RGB', output_format='auto')
-st.title("Youtube_Data_Analysis")
+st.title(:red"Youtube_Channel_Analysis")
 st.text("Welcome EVERYONE")
 
 Question = st.selectbox(
